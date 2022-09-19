ae319fb84db7c70cfc49a31c64b0aa96c257628d
patrikduch <duchpatrik@icloud.com>
2022-09-19 17:31:12 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Todo from './components/Todo';
+
+// rework this into regular api call, feel free to use any open api
+var todos = (): Promise<{id: string; title: string;}[]> => new Promise((res) => {
+  setTimeout(() => {
+    res([
+      {
+        id: "1",
+        title: "Go shopping",
+      },
+      {
+        id: "2",
+        title: "Job interview",
+      },
+      {
+        id: "3",
+        title: "Prepare homework",
+      },
+    ]);
+  }, 100);
+});
+
+function App() {
+  const [state, setState] = React.useState<{ id: string; title: string }[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      var awaitedTodos = await todos();
+      setState([...awaitedTodos])
+    })()
+  }, [])
+
+  return (
+    <div>
+      {state.map((todo) => (
+        <Todo todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
